8a5b94135231b135f1a8d02311a195ca9c60a415
Vitalii-Saienko <vitaliy5saie@gmail.com>
2022-12-24 12:28:04 +0100

    this commit contains extra home-work, dedicated to lesson 4

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,3 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
-// start from here:
-//https://github.com/tel-ran-de/49-49-morning-basic-java/blob/main/src/main/java/homework/lesson4/Task_22.txt
